e96a337dca300d875aa6b48300dd2186c3b25b8b
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2023-02-14 00:40:23 +0530

    changed in Navigation and Calendar.

--- app/src/main/java/com/college/anwesha2k23/MainActivity.kt
+++ app/src/main/java/com/college/anwesha2k23/MainActivity.kt
@@ -20,9 +20,6 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var actionBarToggle: ActionBarDrawerToggle
-    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,82 +27,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        drawerLayout = binding.drawerLayout
-        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
-        drawerLayout.addDrawerListener(actionBarToggle)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBarToggle.syncState()
-
-        binding.navBar.setOnClickListener {
-            drawerLayout.openDrawer(Gravity.LEFT)
-        }
-
         val navView: BottomNavigationView = binding.bottomNavigation
         val navController = findNavController(R.id.fragmentContainer)
         navView.setupWithNavController(navController)
 
-        selectingItems()
-
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        drawerLayout.openDrawer(navView)
-        return true
-    }
-
-    // override the onBackPressed() function to close the Drawer when the back button is clicked
-    override fun onBackPressed() {
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    private fun selectingItems() {
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.sponsor -> {
-                    Toast.makeText(this, "sponsor clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.Team -> {
-                    Toast.makeText(this, "Team clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.Feedback -> {
-                    Toast.makeText(this, "Feedback clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.FAQ -> {
-                    Toast.makeText(this, "FAQ clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.Abouts -> {
-                    Toast.makeText(this, "Abouts clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.logout -> {
-                    signOut()
-                    true
-                }
-                R.id.ca -> {
-                    val intent = Intent(this, CaActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-
-    private fun signOut() {
-        Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-    }
 
 }
 
--- app/src/main/java/com/college/anwesha2k23/home/HomeFragment.kt
+++ app/src/main/java/com/college/anwesha2k23/home/HomeFragment.kt
@@ -1,18 +1,30 @@
 package com.college.anwesha2k23.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.lottie.LottieDrawable
+import com.college.anwesha2k23.LoginActivity
 import com.college.anwesha2k23.R
+import com.college.anwesha2k23.campusAmbassador.CaActivity
 import com.college.anwesha2k23.databinding.FragmentHomeBinding
 import com.college.anwesha2k23.events.EventAdapter
 import com.college.anwesha2k23.events.EventList
+import com.college.anwesha2k23.home.functions.get_events
+import com.college.anwesha2k23.home.functions.nav_items_functions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.navigation.NavigationView
 
 
 class HomeFragment : Fragment() {
@@ -20,12 +32,9 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: EventAdapter
     private lateinit var newEventView : RecyclerView
     private lateinit var newEventList : ArrayList<EventList>
-    private lateinit var eventPoster : Array<Int>
-    private lateinit var eventName : Array<String>
-    private lateinit var eventLocation :  Array<String>
-    private lateinit var eventDate : Array<String>
-    private lateinit var eventTime : Array<String>
-
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,15 +42,31 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater,container,false)
 
+
+        newEventList = arrayListOf()
+        drawerLayout = binding.frameLayout
+        actionBarToggle = ActionBarDrawerToggle(activity, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarToggle)
+        actionBarToggle.syncState()
+
+        binding.navBar.setOnClickListener {
+            drawerLayout.openDrawer(Gravity.LEFT)
+        }
+
         val bottomSheet = binding.eventBottomSheet
         val behavior = BottomSheetBehavior.from(bottomSheet)
         behavior.peekHeight = 400
         behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        nav_items_functions(binding, requireActivity()).selectingItems()
+
         return binding.root
+
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        getUserData()
+
+        get_events(binding, requireActivity()).getUserData(newEventList)
         val layoutManager = LinearLayoutManager(context)
         newEventView = binding.eventsList
         newEventView.layoutManager = layoutManager
@@ -52,39 +77,23 @@ class HomeFragment : Fragment() {
         setAnime()
     }
 
+    fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(navView)
+        return true
+    }
+
+    // override the onBackPressed() function to close the Drawer when the back button is clicked
+    fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            requireActivity().onBackPressed()
+        }
+    }
+
     fun setAnime() {
         binding.animationView.setAnimation(R.raw.map_replace)
         binding.animationView.repeatCount = LottieDrawable.INFINITE
         binding.animationView.playAnimation()
     }
-
-    private fun getUserData() {
-        newEventList = arrayListOf()
-        eventPoster = arrayOf(
-            R.drawable.poster,
-            R.drawable.event
-        )
-        eventName = arrayOf(
-            "DJ Night",
-            "To be update"
-        )
-        eventLocation = arrayOf(
-            "Near Block 3",
-            "To be update"
-        )
-        eventDate = arrayOf(
-            "29 September 2022",
-            "4 December 2022"
-        )
-        eventTime = arrayOf(
-            "18:00",
-            "To be update"
-        )
-        for(i in eventPoster.indices){
-            val event = EventList(eventPoster[i], eventName[i], eventLocation[i], eventDate[i], eventTime[i])
-            newEventList.add(event)
-        }
-    }
-
-
 }
