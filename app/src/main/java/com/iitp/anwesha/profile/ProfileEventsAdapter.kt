64edd8d51ea60d5e906a614ced733df633423a72
Arvind <arvindri2005@gmail.com>
2023-03-12 01:18:16 +0530

    Single event now called througn event Id

--- app/src/main/java/com/iitp/anwesha/profile/ProfileEventsAdapter.kt
+++ app/src/main/java/com/iitp/anwesha/profile/ProfileEventsAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.iitp.anwesha.databinding.MyEventDesignBinding
-import com.iitp.anwesha.home.EventList
 import com.iitp.anwesha.profile.MyEventDetails
 import org.w3c.dom.Text
 import java.text.SimpleDateFormat
